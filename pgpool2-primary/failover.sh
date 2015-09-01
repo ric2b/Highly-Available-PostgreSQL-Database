428ef0be8b16dfb18c5bd259f93d1da788913c3b
ric2b <ricardo.amendoeira@ist.utl.pt>
2015-09-01 14:54:31 +0100

    created the skeleton of the auto failover script

---
+++ pgpool2-primary/failover.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+# please enter your config stuff
+
+promoteCommand="touch HERE!"
+redirectCommand="touch THERE!"
+
+slave1IP="192.168.129.143"
+slave1Port="5432"
+
+slave2IP="192.168.129.202"
+slave2Port="5432"
+
+pcpport="9898"
+pcpuser="postgres"
+pcppass="postgres"
+
+# let's find out what server is ahead (if any) and promote it
+
+slave1LastLog=$(psql -U postgres -h $slave1IP -p $slave1Port -t -c "select pg_last_xlog_replay_location();")
+slave2LastLog=$(psql -U postgres -h $slave2IP -p $slave2Port -t -c "select pg_last_xlog_replay_location();")
+
+#echo $slave1LastLog
+#echo $slave2LastLog
+
+if [[ ! "$slave1LastLog" < "$slave2LastLog" ]]; then
+    echo "one is ahead! (or the same)"
+    promotedServerIP=$slave1IP
+    redirectedServerIP=$slave2IP
+    reattachID=2
+else
+    echo "one is behind"
+    promotedServerIP=$slave2IP
+    redirectedServerIP=$slave1IP
+    reattachID=1
+fi
+
+ssh admra@$slave1IP  "$promoteCommand"
+
+# now that we have a new master, let's point the other one to it
+
+tmpfile=tmpfilerandomstufftomakesureimnotoverwritingyourfiles2JKHEGR7Q23
+
+echo "standby_mode=on" > $tmpfile 
+echo "trigger_file='/tmp/promotedb'" >> $tmpfile
+echo "primary_conninfo='host=$slave1IP port=slave1Port user=replicador application_name=postgresql$(($reattachID+1))'" >> $tmpfile
+
+scp $tmpfile postgres@$redirectedServerIP:/tmp/promotedbTEST
+ssh admra@$slave2IP "$redirectCommand"
+
+# the backend stuff is done, now we need to reattach the redirected server to the user facing pgpool
+echo "pcp_attach_node 10 localhost $pcpport $pcpuser $pcppass $reattachID"
+
+rm $tmpfile
